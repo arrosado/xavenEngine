009a00950fa9556edfdb71aa286881b90cfdb5de
Ariel Rosado Rivera <ariel_00720@yahoo.com>
2014-03-13 09:26:27 -0400

    Simplified rendering

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -16,13 +16,8 @@
 #else
 #include <glut.h>
 #endif
-#include <Game/IComponent.h>
-#include <Game/Graphics/GraphicsCommon.h>
-#include <Game/Graphics/IGameCamera.h>
 #include <Game/Graphics/Console/Console.h>
-#include <Game/Graphics/Grid/Grid.h>
 #include <Game/Graphics/Texture2D/Texture2D.h>
-#include <Game/Input/InputManager.h>
 
 
 #define WIDTH 600
@@ -30,39 +25,21 @@
 
 #define UPDATE_INTERVAL .25
 
-float mouseX, mouseY, rotateX, rotateY;
-Grid* grid = NULL;
-IGameCamera *camera = NULL;
 Texture2D *textureLoader = NULL;
 Texture texture;
 
-Vector3Df cam_position = Vector3DfZero;
-Vector3Df cam_rotation = Vector3DfZero;
-
-
 // What's this???? [Start]
 static const GLfloat vertices[] = { -0.5f, -0.33f,
-0.5f, -0.33f,
--0.5f, 0.33f,
-0.5f, 0.33f };
+100.5f, -100.33f,
+-100.5f, 100.33f,
+100.5f, 100.33f };
 
-static const GLubyte colors[] = {
-255, 255, 0, 255,
-0, 255, 255, 255,
-0, 0, 0, 0,
-255, 0, 255, 255
-};
-
-float matrix[9];
-bool dirty = false;
-Vector3Df point;
-Vector3Df rotation;
-Vector3Df rotationPoint;
-Size2Df scale;
-bool flipHorizontally;
-bool flipVertically;
-// What's this???? [End]
-
+//static const GLubyte colors[] = {
+//255, 255, 0, 255,
+//0, 255, 255, 255,
+//0, 0, 0, 0,
+//255, 0, 255, 255
+//};
 
 
 /* Function Prototypes */
@@ -80,108 +57,40 @@ void Draw();
 
 void Draw(void)
 {
-	glClearColor(0.60f, 0.70f, 0.85f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	//glClearColor(0.60f, 0.70f, 0.85f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Working : image->Render();
-    // Working : image->Render(Vector3DfMake(0.0f, 0.0f, 0.0f), true);
-    // Working : image->Render(Vector3DfMake(0.0f, 0.0f, 0.0f));
-    // Working : image->Render(Vector3DfMake(-100.0f, 0.0f, 0.0f), Size2DfMake(0.5f, 0.5f), Vector3DfMake(1.0f, 0.0f, 0.0f));
-    
-    // Modify matrix here! [Start]
-    // If the images point, scale or rotation are changed, it means we need to adjust the
-    // images matrix and transform the vertices.  If dirty is set we also check to see if it is
-    // necessary to adjust the rotation and scale.  If they are 0 then nothing needs to
-    // be done and we can save some cycles.
-    /*if (dirty) {
-        // Load the identity matrix before applying transforming the matrix for this image.  The
-        // order in which the transformations are applied is important.
-        loadIdentityMatrix(matrix);
-		
-        // Translate the position of the image first
-        translateMatrix(matrix, point);
-		
-        // If this image has been configured to be flipped vertically or horizontally
-        // then set the scale for the image to -1 for the appropriate axis and then translate
-        // the image so that the images origin is rendered in the correct place
-        if(flipVertically) {
-            scaleMatrix(matrix, Size2DfMake(1, -1));
-            translateMatrix(matrix, Vector3DfMake(0, (-textureLoader->contentSize.height * scale.height), 0));
-        }
-		
-        if(flipHorizontally) {
-            scaleMatrix(matrix, Size2DfMake(-1, 1));
-            translateMatrix(matrix, Vector3DfMake((-textureLoader->contentSize.width * scale.width), 0, 0));
-        }
-        
-		// No point in calculating a rotation matrix if there is no rotation been set
-        if(rotation.z != 0)
-            rotateMatrix(matrix, rotationPoint, rotation.z);
-        
-        // No point in calculcating scale if no scale has been set.
-		if(scale.width != 1.0f || scale.height != 1.0f)
-            scaleMatrix(matrix, scale);
-        
-        // Transform the images matrix based on the calculations done above
-        transformMatrix(matrix, image->texturedColoredQuad, image->texturedColoredQuadIVA);
-        
-        // Mark the image as now clean
-		dirty = false;
-	}*/
-    // Modify matrix here! [End]
-    
-    grid->Draw(GridType::XGrid);
-    
     Console::Instance()->Draw();
 	
     // Do Rendering here! [Start]
+    glEnable(GL_DEPTH_TEST);
     
-    /*
-	 * Setup how textures should be rendered i.e.
-	 * how a texture with alpha should be rendered
-	 * on top of another texture.
-	 */
-	if (!glIsEnabled(GL_BLEND)) {
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	}
+    glColor3f(255.0f, 0.0f, 0.0f);
+    glPushMatrix();
+    glRectf(-25.0f, -25.0f, 25.0f, 25.0f);
+    glPopMatrix();
+    glFlush();
+    glDisable(GL_DEPTH_TEST);
     
-	if (!glIsEnabled(GL_TEXTURE_2D)) {
-		glEnable(GL_TEXTURE_2D);
-		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_ALPHA);
-	}
-    
-	if (!glIsEnabled(GL_DEPTH_TEST))
-		glEnable(GL_DEPTH_TEST);
-    
-	glEnableClientState(GL_VERTEX_ARRAY);
+	//glEnableClientState(GL_VERTEX_ARRAY);
     //glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
+    //glEnableClientState(GL_COLOR_ARRAY);
     
 	// Populate the vertex, texcoord and colorpointers with our interleaved vertex data
-    glVertexPointer(2, GL_FLOAT, 0, vertices);
+    //glVertexPointer(2, GL_FLOAT, 0, vertices);
     //glTexCoordPointer(2, GL_FLOAT, sizeof(TexturedColoredVertex), &iva[0].textureVertex);
-    glColorPointer(4, GL_UNSIGNED_BYTE, 0, colors);
+    //glColorPointer(4, GL_UNSIGNED_BYTE, 0, colors);
 	
 	//glBindTexture(GL_TEXTURE_2D, texture.texture->name);
 		
     // Now load the indices array with indexes into the IVA, we draw those triangles to the screen.
     //glDrawElements(GL_TRIANGLES, vertexCounter, GL_UNSIGNED_SHORT, ivaIndices);
-	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	//glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     
-	glDisableClientState(GL_VERTEX_ARRAY);
+	//glDisableClientState(GL_VERTEX_ARRAY);
     //glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-    glDisableClientState(GL_COLOR_ARRAY);
-    
-    
-	if (glIsEnabled(GL_BLEND))
-		glDisable(GL_BLEND);
-    
-	if (glIsEnabled(GL_TEXTURE_2D))
-		glDisable(GL_TEXTURE_2D);
-    
-	if (glIsEnabled(GL_DEPTH_TEST))
-		glDisable(GL_DEPTH_TEST);
+    //glDisableClientState(GL_COLOR_ARRAY);
     
     // Do Rendering here! [End]
     
@@ -193,142 +102,10 @@ void Update(int value) {
 	const double delta = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
     static double lastFrameTime = 0.0f;
     
-    InputManager::Instance()->HandleEvents();
-    InputManager ::Instance()->Update((delta - lastFrameTime));
-    
     Console::Instance()->HandleEvents();
     Console::Instance()->Update((delta - lastFrameTime));
 
-    // GameGraphics Handle Events [Start]
-    
-    bool up, down, left, right, cam;
-    
-	float SPEED = 10.0f;
-    
-	InputManager * i = InputManager::Instance();
-    
-	up = i->GetKeyState('i');
-	down = i->GetKeyState('k');
-	right = i->GetKeyState('l');
-	left = i->GetKeyState('j');
-	cam = i->GetKeyState('c');
-    
-	if (i->GetKeyState('z'))
-		camera->Reset();
-    
-	if (up || down || right || left)
-		camera->Move(right && left ? 0.0f : right ? -SPEED/2 : left ? SPEED/2 : 0.0f,
-							 0.0f,
-							 down && up ? 0.0 : up ? SPEED : down ? -SPEED : 0.0f);
-    
-	int m_x = 0;
-	int m_y = 0;
-	int speed = 10;
-	int direction = 1;
-    
-	switch(i->GetJoystick()->Speed.Upward) {
-        case GameJoyStick::eJoyStickSpeeds::Slow:
-            m_y = direction * speed * 1;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Normal:
-            m_y = direction * speed * 2;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Moderate:
-            m_y = direction * speed * 3;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Fast:
-            m_y = direction * speed * 4;
-            break;
-        default:
-            break;
-	}
-    
-	direction = -1;
-    
-	switch(i->GetJoystick()->Speed.Downward) {
-        case GameJoyStick::eJoyStickSpeeds::Slow:
-            m_y = direction * speed * 1;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Normal:
-            m_y = direction * speed * 2;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Moderate:
-            m_y = direction * speed * 3;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Fast:
-            m_y = direction * speed * 4;
-            break;
-        default:
-            break;
-	}
-    
-	direction = -1;
-    
-	switch(i->GetJoystick()->Speed.Leftward) {
-        case GameJoyStick::eJoyStickSpeeds::Slow:
-            m_x = direction * speed * 1;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Normal:
-            m_x = direction * speed * 2;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Moderate:
-            m_x = direction * speed * 3;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Fast:
-            m_x = direction * speed * 4;
-            break;
-        default:
-            break;
-	}
-    
-	direction = 1;
-    
-	switch(i->GetJoystick()->Speed.Rightward) {
-        case GameJoyStick::eJoyStickSpeeds::Slow:
-            m_x = direction * speed * 1;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Normal:
-            m_x = direction * speed * 2;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Moderate:
-            m_x = direction * speed * 3;
-            break;
-        case GameJoyStick::eJoyStickSpeeds::Fast:
-            m_x = direction * speed * 4;
-            break;
-        default:
-            break;
-	}
-    
-	if (cam)
-		camera->Rotate(m_y, m_x, 0.0f);
-
-    
-    // GameGraphics Handle Events [End]
-    
-    camera->Update((delta - lastFrameTime));
-    
     lastFrameTime = delta;
-    
-    static float rotationAngle = 0.0f;
-    
-    bool rotate = InputManager::Instance()->GetKeyState('r');
-    bool clockwise = InputManager::Instance()->GetKeyState('e');
-    
-    if (fabs(rotationAngle) > 360.0f)
-        rotationAngle = 0.0f;
-    
-    if (rotate)
-        rotationAngle += clockwise ? -1 : 1;
-    
-    // ToDo: Rotate image here with the rotation variable
-    rotation = Vector3DfMake(0.0f, 0.0f, rotationAngle);
-    
-    Console* c = Console::Instance();
-	
-	c->Write("Image Rotation = %.1f", rotationAngle);
-	c->Write("Image rotating %s", clockwise ? "Clockwise" : "CounterClockwise");
-    
     
 	glutPostRedisplay();
     
@@ -336,23 +113,8 @@ void Update(int value) {
 }
 
 void HandleResize(int w, int h) {
-	/*
-	* Initialize the Screen Orientation.
-	*
-	* Switch to GL_PROJECTION matrix mode
-	* and reset the current matrix with the
-	* identity matrix
-	*/
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-
-	/*
-	* Switch the orientation of the view port
-	* to match the current device orientation.
-	*
-	* {0, 0} at the middle of the screen.
-	*/
-
 	glViewport(0, 0, w, h);
 
 	GLdouble fov, aspect, zNear, zFar, xmin, xmax, ymin, ymax;
@@ -368,38 +130,19 @@ void HandleResize(int w, int h) {
 	xmax = ymax * aspect;
 
 
-	glFrustum(xmin, xmax, ymin, ymax, zNear, zFar); /* 3D camera type */
+	//glFrustum(xmin, xmax, ymin, ymax, zNear, zFar); /* 3D camera type */
 	//glOrtho(-w/2, w/2, -h/2, h/2, zNear, zFar);
-
+    gluOrtho2D(0.0f, (GLdouble)w, 0.0f, (GLdouble)h);
+    
 	/*
 	* Switch to GL_MODELVIEW so we can now draw our objects
 	*/
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-
-	/* Camera local coordinate system translation.
-	* If moving the camera in its local coordinate system is desired,
-	* then the translation has to be done before any rotation. If
-	* translation is done after rotating, then the translation will
-	* be executed on world coordinates.
-	*/
-	glTranslatef(cam_position.x, cam_position.y, cam_position.z);
-
-	/*
-	* Rotate the projection matrix on the cameras
-	* local coordinate system.
-	*/
-	glRotatef(cam_rotation.x, 1.0f, 0.0f, 0.0f);
-	glRotatef(cam_rotation.y, 0.0f, 1.0f, 0.0f);
-	glRotatef(cam_rotation.z, 0.0f, 0.0f, 1.0f);
-
-	//glTranslatef(this->position.x, this->position.y, this->position.z);
 }
 
 void HandleKeyPress(unsigned char key, int x, int y)
 {
-    if (key >= 0) InputManager::Instance()->SetKeyState(key, true);
-    
     switch (key)
     {
         case 27 :
@@ -409,71 +152,42 @@ void HandleKeyPress(unsigned char key, int x, int y)
     }
 }
 
-void HandleKeyRelease(unsigned char key, int x, int y)
-{
-    if (key >= 0) InputManager::Instance()->SetKeyState(key, false);
-}
+void HandleKeyRelease(unsigned char key, int x, int y) { }
 
-void HandleSpecialKeyPress(int key, int x, int y) {
-    if (key >= 0) InputManager::Instance()->SetKeyState(key, true);
-}
+void HandleSpecialKeyPress(int key, int x, int y) { }
 
-void HandleSpecialKeyRelease(int key, int x, int y) {
-    if (key >= 0) InputManager::Instance()->SetKeyState(key, false);
-}
+void HandleSpecialKeyRelease(int key, int x, int y) { }
 
-void HandleMouse(int button, int state, int x, int y) {
-    InputManager::Instance()->UpdatePointer(button, state, x, y);
-}
+void HandleMouse(int button, int state, int x, int y) { }
 
-void HandleMousePassive(int x, int y){
-    mouseX = x;
-    mouseY = y;
-    InputManager::Instance()->UpdatePointer(-1, false, x, y);
-}
+void HandleMousePassive(int x, int y){ }
 
-void HandleMouseMotion(int x, int y){
-    const float SPEED = 10;
-    
-    rotateX += (mouseX-x)/SPEED;
-    rotateY += (mouseY-y)/SPEED;
-    
-    HandleMousePassive(x, y);
-}
+void HandleMouseMotion(int x, int y){ }
 
 void initGame(int w, int h)
 {
-    camera = CreateGameCamera();
 	HandleResize(w, h);
     
-    InputManager::Instance()->Init(3, w, h, 100);
     Console::Instance()->Init(0);
-
-    grid = new Grid(20, 20, 20);
     
+    /*
     // Load texture [Start]
     textureLoader = new Texture2D(RESOURCES_FOLDER "run.png", GL_LINEAR);
-    
     texture.name = RESOURCES_FOLDER "run.png";
 	texture.texture = textureLoader;
 	texture.retainCount = 0;
 	texture.retainCount++;
     // Load texture [End]
+     */
 }
 
 void endGame()
 {
-    InputManager::Instance()->Quit();
-    InputManager::Instance()->Cleanup();
-    delete InputManager::Instance();
-    
     Console::Instance()->Quit();
     Console::Instance()->Cleanup();
     delete Console::Instance();
     
-    delete grid;
-    delete camera;
-    delete textureLoader;
+    //delete textureLoader;
 }
 
 #ifdef _WIN32
